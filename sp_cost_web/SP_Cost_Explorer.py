753401059a0305136f6a72b941301a07997e5ebf
Kiran Karra <kiran.karra@gmail.com>
2023-08-28 15:09:13 -0700

    updated readme

--- sp_cost_web/SP_Cost_Explorer.py
+++ sp_cost_web/SP_Cost_Explorer.py
@@ -18,9 +18,9 @@ We examine five different SP strategies for participating in the Filecoin networ
 
 1. FIL+: This models a FIL+ SP. Additional costs associated with being a FIL+ miner include: a) storing additional copies, b) increased bandwidth cost, c) business development costs, d) increased pledge collateral costs, and
 2. FIL+ Cheat: This models an SP paying minimal business costs to pass the necessary hurdles to receive FIL+. They do not incur BizDev costs, but also do not receive revenue from clients.
-3. FIL+ Exploit: This models an SP actively exploiting FIL+ and not growing a storage business. They don't incur BizDev costs, do not store an extra copy, have reduced bandwidth capabilities, but also do not receive revenue from clients.
-4. CC: This models a CC SP. CC SPs have reduced costs associated with bandwidth, BizDev, pledge collateral, and data preparation, but also receive less block rewards.
-5. Regular Deal (RD): This models an SP that is growing a storage business *without* FIL+. Their BizDev costs can be lower, and their pledge is lower, but they also receive less block rewards.
+3. FIL+ Exploit: This models an SP actively exploiting FIL+ and not growing a storage business. They don't incur BizDev costs, do not store an extra copy, have reduced bandwidth costs(10% of FIL+), but also do not receive revenue from clients.
+4. CC: This models a CC SP. CC SPs have reduced costs associated with bandwidth (10% of FIL+), BizDev, pledge collateral, and data preparation, but also receive less block rewards.
+5. Regular Deal (RD): This models an SP that is growing a storage business *without* FIL+. Their BizDev and bandwidth costs can be lower, and their pledge is lower, but they also receive less block rewards.
 
 Two interactive calculators are provided. Both have slider bars that allow you to explore how different variables, such as the token exchange rate, costs associated with business development, network power onboarding rates, and data preparation costs, affect each SP strategy's net income. 
 
