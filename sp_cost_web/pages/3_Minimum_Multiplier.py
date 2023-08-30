1e197ddd763c40b1e617f8e2d217dae72dd8fe0e
Kiran Karra <kiran.karra@gmail.com>
2023-08-30 13:58:37 -0700

    updated to show minimum multiplier from a network persepctive

--- sp_cost_web/pages/3_Minimum_Multiplier.py
+++ sp_cost_web/pages/3_Minimum_Multiplier.py
@@ -23,44 +23,26 @@ def generate_plots(minimum_m_df):
 
 def compute_minimum_multiplier(scenario2erpt=None):
     onboarding_scenario = st.session_state['mm_onboarding_scenario'].lower()
-    borrowing_cost_pct = st.session_state['mm_borrow_cost_pct'] / 100.0
-    filp_bd_cost_tib_per_yr = st.session_state['mm_filp_bizdev_cost']
-    deal_income_tib_per_yr = st.session_state['mm_deal_income']
-    data_prep_cost_tib_per_yr = st.session_state['mm_data_prep_cost']
-    penalty_tib_per_yr = st.session_state['mm_cheating_penalty']
+    deal_income = st.session_state['mm_deal_income']
+    cc_cost = st.session_state['mm_cc_cost']
+    deal_cost_multiplier = st.session_state['mm_deal_cost_mutiplier']
 
-    power_cost_tib_per_yr = st.session_state['mm_power_cost']
-    bw_cost_tib_per_yr = st.session_state['mm_bw_cost']
-    staff_cost_tib_per_yr = st.session_state['mm_staff_cost']
-
-    sealing_costs_tib_per_yr, gas_cost_tib_per_yr, _, _ = utils.get_negligible_costs(bw_cost_tib_per_yr)
+    erpt = scenario2erpt[onboarding_scenario]
 
     # sweep across a) exchange rate
-    exchange_rate_vec = np.linspace(3, 20.0, 100)
+    exchange_rate_vec = np.linspace(3, 50.0, 100)
     
     minimum_m_results = []
     for cost_scaling in [1.0, 0.8, 1.2]:
-        erpt = scenario2erpt[onboarding_scenario]
+        
         for exchange_rate in exchange_rate_vec:
-            sector_return_nomult = erpt*exchange_rate
-            revenue = deal_income_tib_per_yr
+            cc_cost_full = cost_scaling*cc_cost
+            cc_profit = erpt*exchange_rate - cc_cost_full
+            # deal_profit = deal_income + m*erpt*exchange_rate - deal_cost_multiplier*cc_cost_full
+            # deal_income + m*erpt*exchange_rate - deal_cost_multiplier*cc_cost_full = erpt_exchange_rate-cc_cost_full
+            # => m = (cc_profit - deal_income + deal_cost_multiplier*cc_cost_full)/(erpt*exchange_rate)
+            minimum_m = (cc_profit - deal_income + deal_cost_multiplier*cc_cost_full)/(erpt*exchange_rate)
 
-            cost_multiplier = sector_return_nomult*borrowing_cost_pct*cost_scaling
-            cost_no_multiplier = (
-                gas_cost_tib_per_yr +
-                power_cost_tib_per_yr +
-                bw_cost_tib_per_yr +
-                staff_cost_tib_per_yr +
-                sealing_costs_tib_per_yr +
-                data_prep_cost_tib_per_yr +
-                filp_bd_cost_tib_per_yr +
-                (staff_cost_tib_per_yr+power_cost_tib_per_yr)*0.5 +
-                penalty_tib_per_yr
-            ) * cost_scaling
-
-            # cc_net_income = sector_return_nomult - (cost_multiplier + cost_no_multiplier)
-            # minimum_m = (cc_net_income + cost_no_multiplier - revenue + sector_return_nomult) / (sector_return_nomult - cost_multiplier)
-            minimum_m = (sector_return_nomult - cost_multiplier - revenue)/(sector_return_nomult - cost_multiplier)
             minimum_m_results.append({
                 'cost_scaling': cost_scaling,
                 'cost_scaling_str': f'{cost_scaling}x',
@@ -85,47 +67,20 @@ with st.sidebar:
     st.selectbox(
         'Onboarding Scenario', ('Status-Quo', 'Pessimistic', 'Optimistic'), key="mm_onboarding_scenario",
         on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-    )                
-    with st.expander("Revenue Settings", expanded=False):
-        st.slider(
-            'Deal Income ($/TiB/Yr)', 
-            min_value=0.0, max_value=100.0, value=16.0, step=1.0, format='%0.02f', key="mm_deal_income",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-    with st.expander("Cost Settings", expanded=False):
-        st.slider(
-            'Borrowing Costs (Pct. of Pledge)', 
-            min_value=0.0, max_value=100.0, value=50.0, step=1.00, format='%0.02f', key="mm_borrow_cost_pct",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'FIL+ Biz Dev Cost ($/TiB/Yr)', 
-            min_value=1.0, max_value=50.0, value=8.0, step=1.0, format='%0.02f', key="mm_filp_bizdev_cost",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'Data Prep Cost ($/TiB/Yr)', 
-            min_value=0.0, max_value=50.0, value=1.0, step=1.0, format='%0.02f', key="mm_data_prep_cost",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'Cheating Penalty ($/TiB/Yr)', 
-            min_value=0.0, max_value=50.0, value=18.0, step=1.0, format='%0.02f', key="mm_cheating_penalty",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'Power+COLO Cost ($/TiB/Yr)', 
-            min_value=0.0, max_value=50.0, value=6.0, step=1.0, format='%0.02f', key="mm_power_cost",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'Bandwidth [10GBPS] Cost ($/TiB/Yr)', 
-            min_value=0.0, max_value=50.0, value=6.0, step=1.0, format='%0.02f', key="mm_bw_cost",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
-        st.slider(
-            'Staff Cost ($/TiB/Yr)', 
-            min_value=0.0, max_value=50.0, value=8.0, step=1.0, format='%0.02f', key="mm_staff_cost",
-            on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
-        )
+    )     
+    st.slider(
+        'Deal Income ($/TiB/Yr)', 
+        min_value=0.0, max_value=100.0, value=16.0, step=1.0, format='%0.02f', key="mm_deal_income",
+        on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
+    )
+    st.slider(
+        'CC Sector Cost ($/TiB/Yr)', 
+        min_value=0.0, max_value=100.0, value=16.0, step=1.0, format='%0.02f', key="mm_cc_cost",
+        on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
+    )
+    st.slider(
+        'Deal Sector Cost Multiplier (X)', 
+        min_value=1.0, max_value=10.0, value=2.0, step=1.0, format='%0.02f', key="mm_deal_cost_multiplier",
+        on_change=compute_minimum_multiplier, kwargs=kwargs, disabled=False, label_visibility="visible"
+    )
     st.button("Compute!", on_click=compute_minimum_multiplier, kwargs=kwargs, key="forecast_button")
